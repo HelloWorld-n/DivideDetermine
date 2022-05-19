020deebdd7b7bde2611c06c41e928c5ac9140c41
HelloWorld-n <gajica.b@gmail.com>
2022-05-19 21:13:09 +0200

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+from math import sqrt, ceil, floor
+
+class DivideDetermine:
+	@staticmethod
+	def main(n):
+		print(1)
+		print()
+		tm = []
+		sroot = sqrt(n)
+		for i in range(2, ceil(sroot)):
+			if n % i == 0:
+				print(i)
+				tm = [i] + tm
+		if floor(sroot) == ceil(sroot):
+			print(floor(sroot))
+		for i in tm:
+			print(n // i)
+			
+		print()
+		print(n)
+		
+
+
+if __name__ == "__main__":
+	DivideDetermine.main(int(eval(input())))
