adebdcb65f90b7df6695d93f1d3167aac2eb571c
HelloWorld-n <gajica.b@gmail.com>
2022-05-23 18:58:00 +0200

    modified:   README.md 	modified:   main.py

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # DivideDetermine
 
-Follow instructions。
+When prompted for integer of choice do。  
+&nbsp; &nbsp; If you want to exit。  
+&nbsp; &nbsp; &nbsp; &nbsp; Write `exit()`。  
+&nbsp; &nbsp; Else。  
+&nbsp; &nbsp; &nbsp; &nbsp; Write integer you want to determine。  
+&nbsp; &nbsp; Fi。  
+Done。  
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from math import sqrt, ceil, floor
 class DivideDetermine:
 	@staticmethod
 	def main(n):
+		if n < 1:
+			raise Exception(f"Excepted positive integer、 got {n} instead。")
 		print(1)
 		tm = []
 		sroot = sqrt(n)
@@ -16,9 +18,9 @@ class DivideDetermine:
 		for i in tm:
 			print(n // i)
 		print(n)
-		
+
 while __name__ == "__main__":
-	try:	
-		DivideDetermine.main(int(eval(input("Enter integer of choice: "))))
+	try:
+		DivideDetermine.main(int(eval(input("Enter positive integer of choice: "))))
 	except Exception as exception:
 		print(exception)
